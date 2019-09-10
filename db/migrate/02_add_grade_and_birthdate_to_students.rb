0cd5b8b02987d3733055ce8de2c5ccfc0ce533af
Robinson Regalado <rregalado17@gmail.com>
2019-09-10 00:43:35 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,15 @@
+<<<<<<< HEAD
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 
   def change
     add_column :students, :grade, :integer
     add_column :students, :birthdate, :string
+=======
+AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
+
+  def change 
+    add_column :students, :grade, :integer, :birthdate, :string
+>>>>>>> db93241b4f5d04136488415a2261769246768a86
   end
 
 end
--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
 
   def change
     change_column :students, :birthdate, :datetime
+=======
+ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+
+  def change_column(table_name, column_name, type) 
+    add_column :students, :birthdate, :datetime
+>>>>>>> db93241b4f5d04136488415a2261769246768a86
   end
 
 end
